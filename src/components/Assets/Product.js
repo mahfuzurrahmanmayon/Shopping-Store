b00cabe8ec85e7fe5802354678a145773361eb29
Mahfuzur Rahman <mahfujurrahman291003@gmail.com>
2024-02-03 14:31:13 +0600

    Add asset to image and product array of objects

---
+++ src/components/Assets/Product.js
@@ -0,0 +1,274 @@
+import p1_img from "../components/P-01.jpg"
+import p2_img from "../components/P-02.jpg"
+import p3_img from "../components/P-03.jpg"
+import p4_img from "../components/P-04.jpg"
+import p5_img from "../components/P-05.jpg"
+import p6_img from "../components/P-06.jpg"
+import p7_img from "../components/P-07.jpg"
+import p8_img from "../components/P-08.jpg"
+import p9_img from "../components/P-09.jpg"
+import p10_img from "../components/P-10.jpg"
+import p11_img from "../components/P-11.jpg"
+import p12_img from "../components/P-12.jpg"
+import p13_img from "../components/P-13.jpg"
+import p14_img from "../components/P-14.jpg"
+import p15_img from "../components/P-15.jpg"
+import p16_img from "../components/P-16.jpg"
+import p17_img from "../components/P-17.jpg"
+import p18_img from "../components/P-18.jpg"
+import p19_img from "../components/P-19.jpg"
+import p20_img from "../components/P-20.jpg"
+import p21_img from "../components/P-21.jpg"
+import p22_img from "../components/P-22.jpg"
+import p23_img from "../components/P-23.jpg"
+import p24_img from "../components/P-24.jpg"
+import p25_img from "../components/P-25.jpg"
+import p26_img from "../components/P-26.jpg"
+import p27_img from "../components/P-27.jpg"
+import p28_img from "../components/P-28.jpg"
+import p29_img from "../components/P-29.jpg"
+import p30_img from "../components/P-30.jpg"
+
+let all_products = [
+    {
+      id: 1,
+      name: "Elegant Silver Mesh Strap Analog Watch",
+      category: "women",
+      image: p1_img,
+      rating: 4.5,
+      price: 50.0,
+    },
+    {
+      id: 2,
+      name: "Classic Rose Gold Leather Band Watch",
+      category: "women",
+      image: p2_img,
+      rating: 4.2,
+      price: 85.0,
+    },
+    {
+      id: 3,
+      name: "Minimalist Black Dial Stainless Steel Watch",
+      category: "women",
+      image: p3_img,
+      rating: 4.7,
+      price: 60.0,
+    },
+    {
+      id: 4,
+      name: "Stylish Two-Tone Bracelet Wristwatch",
+      category: "women",
+      image: p4_img,
+      rating: 4.0,
+      price: 100.0,
+    },
+    {
+      id: 5,
+      name: "Chic Gold and White Ceramic Watch",
+      category: "women",
+      image: p5_img,
+      rating: 4.5,
+      price: 85.0,
+    },
+    {
+      id: 6,
+      name: "Fashionable Rose Gold Square Face Watch",
+      category: "women",
+      image: p6_img,
+      rating: 4.2,
+      price: 85.0,
+    },
+    {
+      id: 7,
+      name: "Classic Round Dial Leather Strap Watch",
+      category: "women",
+      image: p7_img,
+      rating: 4.8,
+      price: 85.0,
+    },
+    {
+      id: 8,
+      name: "Elegant Diamond-studded Ladies Watch",
+      category: "women",
+      image: p8_img,
+      rating: 4.3,
+      price: 85.0,
+    },
+    {
+      id: 9,
+      name: "Trendy Silver and Blue Analog Watch",
+      category: "women",
+      image: p9_img,
+      rating: 4.7,
+      price: 85.0,
+    },
+    {
+      id: 10,
+      name: "Modern Square Face Digital Wristwatch",
+      category: "women",
+      image: p10_img,
+      rating: 4.5,
+      price: 85.0,
+    },
+    {
+      id: 11,
+      name: "Sophisticated Gold Link Bracelet Watch",
+      category: "women",
+      image: p11_img,
+      rating: 4.6,
+      price: 85.0,
+    },
+    {
+      id: 12,
+      name: "Sporty Pink Silicone Band Digital Watch",
+      category: "women",
+      image: p12_img,
+      rating: 4.1,
+      price: 85.0,
+    },
+    {
+      id: 13,
+      name: "Classic Black Leather Strap Analog Watch",
+      category: "men",
+      image: p13_img,
+      rating: 4.5,
+      price: 85.0,
+    },
+    {
+      id: 14,
+      name: "Sleek Silver Stainless Steel Bracelet Watch",
+      category: "men",
+      image: p14_img,
+      rating: 4.2,
+      price: 85.0,
+    },
+    {
+      id: 15,
+      name: "Bold Chronograph Black Dial Men's Watch",
+      category: "men",
+      image: p15_img,
+      rating: 4.7,
+      price: 85.0,
+    },
+    {
+      id: 16,
+      name: "Military Style Nylon Strap Analog Watch",
+      category: "men",
+      image: p16_img,
+      rating: 4.0,
+      price: 85.0,
+    },
+    {
+      id: 17,
+      name: "Rugged Brown Leather Band Field Watch",
+      category: "men",
+      image: p17_img,
+      rating: 4.5,
+      price: 85.0,
+    },
+    {
+      id: 18,
+      name: "Stylish Blue Dial Stainless Steel Watch",
+      category: "men",
+      image: p18_img,
+      rating: 4.3,
+      price: 85.0,
+    },
+    {
+      id: 19,
+      name: "Modern Dual-Time Zone Men's Wristwatch",
+      category: "men",
+      image: p19_img,
+      rating: 4.8,
+      price: 85.0,
+    },
+    {
+      id: 20,
+      name: "Luxurious Gold Plated Men's Chronograph",
+      category: "men",
+      image: p20_img,
+      rating: 4.2,
+      price: 85.0,
+    },
+    {
+      id: 21,
+      name: "Classic Brown Leather Strap Analog Watch",
+      category: "men",
+      image: p21_img,
+      rating: 4.7,
+      price: 85.0,
+    },
+    {
+      id: 22,
+      name: "Sleek Silver Mesh Bracelet Men's Watch",
+      category: "men",
+      image: p22_img,
+      rating: 4.3,
+      price: 85.0,
+    },
+    {
+      id: 23,
+      name: "Bold Black and Red Sports Chronograph",
+      category: "men",
+      image: p23_img,
+      rating: 4.6,
+      price: 85.0,
+    },
+    {
+      id: 24,
+      name: "Rugged Outdoor Waterproof Men's Watch",
+      category: "men",
+      image: p24_img,
+      rating: 4.1,
+      price: 85.0,
+    },
+    {
+      id: 25,
+      name: "Colorful Fun Patterned Kids Wristwatch",
+      category: "kid",
+      image: p25_img,
+      rating: 4.5,
+      price: 85.0,
+    },
+    {
+      id: 26,
+      name: "Cartoon Character Printed Kids Watch",
+      category: "kid",
+      image: p26_img,
+      rating: 4.2,
+      price: 85.0,
+    },
+    {
+      id: 27,
+      name: "Adorable Animal Design Children's Watch",
+      category: "kid",
+      image: p27_img,
+      rating: 4.7,
+      price: 85.0,
+    },
+    {
+      id: 28,
+      name: "Sporty Digital Display Kids Wristwatch",
+      category: "kid",
+      image: p28_img,
+      rating: 4.0,
+      price: 85.0,
+    },
+    {
+      id: 29,
+      name: "Colorful Silicone Strap Children's Watch",
+      category: "kid",
+      image: p29_img,
+      rating: 4.5,
+      price: 85.0,
+    },
+    {
+      id: 30,
+      name: "Cool Space Theme Kids Analog Watch",
+      category: "kid",
+      image: p30_img,
+      rating: 4.2,
+      price: 85.0,
+    },
+];
+  
