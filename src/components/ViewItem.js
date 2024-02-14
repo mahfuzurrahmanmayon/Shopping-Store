46f4db4ffec9d08a2c2178c8eb434412430179c4
Mahfuzur Rahman <mahfujurrahman291003@gmail.com>
2024-02-14 17:50:29 +0600

    viewItem Page and Increment Decrement Function

--- src/components/ViewItem.js
+++ src/components/ViewItem.js
@@ -7,12 +7,32 @@ const ViewItem = () => {
   const { productId } = useParams();
   console.log("productID", productId)
   const [productData, setProductData] = useState(null);
+  const [counter, setCounter] = useState(1)
+  const [addToCart, setAddToCart] = useState([])
 
   useEffect(() => {
-    const filteredProductData = all_products.filter((product) => product.id === productId);
+    const filteredProductData = all_products.filter((product) => product.id === parseInt(productId));
     console.log('Filtered products:', filteredProductData[0]);
     setProductData(filteredProductData.length > 0 ? filteredProductData[0] : null);
   }, [productId]);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleDecrement = () => {
+    if(counter > 1){
+      setCounter(counter - 1)
+    }
+  }
+
+  const handleAddToCart = () => {
+    const filteredProductAddData = all_products.filter((product) => product.id === parseInt(productId));
+    console.log(filteredProductAddData)
+    const updatedCart = [...addToCart];
+    updatedCart.push(filteredProductAddData[0]);
+    console.log("AddToCart Array:: ",updatedCart)
+  }
 
   return (
     <div className='view-item-section'>
@@ -29,11 +49,11 @@ const ViewItem = () => {
                 <p><strong>Rating:</strong> {productData.rating}</p>
                 <p><strong>Category:</strong> {productData.category}</p>
                 <div className='quantity'>
-                  <div className='update-number'>-</div>
-                  <input className='update-input' type="number" min="0" />
-                  <div className='update-number'>+</div>
+                  <div onClick={() => handleDecrement()} className='update-number'>-</div>
+                  <div className='update-input'>{counter}</div>
+                  <div onClick={() => handleIncrement()} className='update-number'>+</div>
                 </div>
-                <button className='product-details-btn w-inline-block mt-30'>Add To Cart</button>
+                <button onClick={() => handleAddToCart()} className='product-details-btn w-inline-block mt-30'>Add To Cart</button>
               </>
             ) : (
               <p>No product found with ID: {productId}</p>
